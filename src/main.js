6b43f28c5962297b0c611163315fa9434d0ac491
nguyenkimtuyen2024 <tuyenvt23v7x339@dttx.ctu.edu.vn>
2025-04-29 16:46:09 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import router from './router'
+createApp(App).use(router).mount("#app");
 
 const app = createApp(App)
 
