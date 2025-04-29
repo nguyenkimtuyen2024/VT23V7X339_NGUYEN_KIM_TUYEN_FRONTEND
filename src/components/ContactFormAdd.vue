22538219bd97d91b7f8a27e8892e2eb7bb111fe2
nguyenkimtuyen2024 <tuyenvt23v7x339@dttx.ctu.edu.vn>
2025-04-29 20:01:00 +0700

    them trang them lien he

---
+++ src/components/ContactFormAdd.vue
@@ -0,0 +1,104 @@
+<template>
+    <Form @submit="submitContact" :validation-schema="contactFormSchema">
+        <div class="form-group">
+            <label for="name">Tên</label>
+            <Field name="name" type="text" class="form-control" v-model="contactLocal.name" />
+            <ErrorMessage name="name" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <Field name="email" type="email" class="form-control" v-model="contactLocal.email" />
+            <ErrorMessage name="email" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="address">Địa chỉ</label>
+            <Field name="address" type="text" class="form-control" v-model="contactLocal.address" />
+            <ErrorMessage name="address" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="phone">Điện thoại</label>
+            <Field name="phone" type="tel" class="form-control" v-model="contactLocal.phone" />
+            <ErrorMessage name="phone" class="error-feedback" />
+        </div>
+        <div class="form-group form-check">
+            <input name="favorite" type="checkbox" class="form-check-input" v-model="contactLocal.favorite" />
+            <label for="favorite" class="form-check-label">
+                <strong>Liên hệ yêu thích</strong>
+            </label>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary">Lưu</button>
+            <button type="button" class="ml-2 btn btn-danger" @click="Cancel">
+                Thoát
+            </button>
+        </div>
+    </Form>
+</template>
+
+<script>
+import * as yup from "yup";
+import { Form, Field, ErrorMessage } from "vee-validate";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        // eslint-disable-next-line vue/no-reserved-component-names
+        Form,
+        Field,
+        ErrorMessage,
+    },
+    data() {
+        const contactFormSchema = yup.object().shape({
+            name: yup
+                .string()
+                .required("Tên phải có giá trị.")
+                .min(2, "Tên phải ít nhất 2 ký tự.")
+                .max(50, "Tên có nhiều nhất 50 ký tự."),
+            email: yup
+                .string()
+                .email("E-mail không đúng.")
+                .max(50, "E-mail tối đa 50 ký tự."),
+            address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
+            phone: yup
+                .string()
+                .matches(
+                    /((09|03|07|08|05)+([0-9]{8})\b)/g,
+                    "Số điện thoại không hợp lệ."
+                ),
+        });
+
+        return {
+            contactLocal: {
+                name: '',
+                email: '',
+                address: '',
+                phone: '',
+                favorite: false,
+            },
+            contactFormSchema,
+        };
+    },
+    methods: {
+        async submitContact() {
+            try {
+                await ContactService.create(this.contactLocal);
+                alert('Liên hệ đã được tạo mới thành công.');
+                this.$router.push({ name: "contactbook" }); 
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        Cancel() {
+            const reply = window.confirm('Bạn có muốn rời khỏi trang mà không lưu lại không?');
+            if (!reply) {
+                return false; 
+            }
+            this.$router.push({ name: "contactbook" }); 
+        }
+    },
+};
+</script>
+
+<style scoped>
+@import "@/assets/form.css";
+</style>
